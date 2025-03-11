c3191973736c62f80d97e3e509378046c95f624a
Pierre Pontet <ppontet@student.42lyon.fr>
2025-03-11 14:40:28 +0100

    Ex03 WIP, more basics and cleaned

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,161 +6,57 @@
 /*   By: ppontet <ppontet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 12:32:55 by ppontet           #+#    #+#             */
-/*   Updated: 2025/03/11 12:32:57 by ppontet          ###   ########lyon.fr   */
+/*   Updated: 2025/03/11 14:29:56 by ppontet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 #include <cmath>
 
-const int Point::nbBits = 8;
-
-Point::Point() 
+Point::Point() : x(0), y(0)
 {
-  // std::cout << std::endl;
-  // std::cout << "Default constructor called" << std::endl;
-  this->value = 0;
+  ;
 }
 
 Point::~Point() 
 {
-  // std::cout << "Destructor called" << std::endl;
+  ;
 }
 
-Point::Point(const Point &f) 
+Point::Point(const Point &f) : x(f.x), y(f.y)
 {
-  // std::cout << "Copy constructor called" << std::endl;
-  *this = f;
+  ;
 }
 
-Point::Point(const int value) 
+Point::Point(const int x_value, const int y_value) : x(x_value), y(y_value)
 {
-  // std::cout << "Int constructor called" << std::endl;
-  this->setRawBits(value << 8);
+  ;
 }
 
-Point::Point(const float value) 
+Point::Point(const float x_value, const float y_value) : x(x_value), y(y_value) 
 {
-  // std::cout << "Float constructor called" << std::endl;
-  this->setRawBits(roundf(value * (1 << nbBits)));
+  ;
 }
 
-Point &Point::operator=(const Point &other) 
+Point &Point::operator=(const Point &other)
 {
-  // std::cout << "Copy assignment operator called" << std::endl;
-  this->value = other.getRawBits();
+  // Can't modify x and y as they are const
+  (void)other;
   return (*this);
 }
 
-std::ostream &operator<<(std::ostream &o, Point const &Point) 
-{
-  o << Point.toFloat();
-  return (o);
-}
-
-// Pre-Increment
-Point &Point::operator++(void) 
-{
-  this->setRawBits(this->getRawBits() + 1);
-  return (*this);
-}
-
-// Post-Increment (int dummy, exist only to differentiate with pre-increment)
-Point Point::operator++(int)
-{
-  Point temp(*this);
-
-  this->setRawBits(this->getRawBits() + 1);
-  return (temp);
-}
-
-// Pre-Decrement
-Point &Point::operator--(void) 
-{
-  this->setRawBits(this->getRawBits() - 1);
-  return (*this);
-}
-
-// Post-Decrement (int dummy, exist only to differentiate with pre-decrement)
-Point Point::operator--(int)
-{
-  Point temp(*this);
-
-  this->setRawBits(this->getRawBits() - 1);
-  return (temp);
-}
-
-Point Point::operator*(const Point &other)
-{
-  return (this->toFloat() * other.toFloat());
-}
-
-Point Point::operator/(const Point &other)
-{
-  return (this->toFloat() / other.toFloat());
-}
-
-Point Point::operator+(const Point &other)
-{
-  return (this->toFloat() + other.toFloat());
-}
-
-Point Point::operator-(const Point &other)
-{
-  return (this->toFloat() - other.toFloat());
-}
-
-bool Point::operator<(const Point &rhs) const { return (this->value < rhs.value); }
-bool Point::operator> (const Point &rhs) const { return ( rhs < *this); }
-bool Point::operator<=(const Point &rhs) const { return !(*this > rhs); }
-bool Point::operator>=(const Point &rhs) const { return !(*this < rhs); }
-bool Point::operator==(const Point &rhs) const { return (this->value == rhs.value); }
-bool Point::operator!=(const Point &rhs) const { return !(*this == rhs); }
-
-void Point::setRawBits(int const raw) 
+bool Point::operator==(const Point &rhs) const
 { 
-  this->value = raw; 
+  if (this->x == rhs.x && this->y == rhs.y)
+    return (1);
+  return (0);
 }
 
-int Point::getRawBits(void) const 
-{ 
-  return (this->value); 
-}
-
-float Point::toFloat(void) const 
+Fixed Point::getX(void)
 {
-  return (this->getRawBits() / (float)(1 << nbBits));
+  return (this->x);
 }
-
-float Point::toInt(void) const 
-{ 
-  return (this->getRawBits() >> nbBits); 
-}
-
-Point Point::max(const Point &a, const Point &b)
+Fixed Point::getY(void)
 {
-  if (a.toFloat() > b.toFloat())
-    return (a);
-  return (b);
-}
-
-Point Point::max(Point &a, Point &b)
-{
-  if (a.toFloat() > b.toFloat())
-    return (a);
-  return (b);
-}
-
-Point Point::min(const Point &a, const Point &b)
-{
-  if (a.toFloat() < b.toFloat())
-    return (a);
-  return (b);
-}
-
-Point Point::min(Point &a, Point &b)
-{
-  if (a.toFloat() < b.toFloat())
-    return (a);
-  return (b);
+  return (this->y);
 }
--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -8,42 +8,19 @@ class Point{
 	private:
 		Fixed const x;
 		Fixed const y;
-		int value;
-		static const int nbBits;
 
 	public:
 		Point();
 		~Point();
 		Point(const Point &f);
-		Point(const int value);
-		Point(const float value);
+		Point(const int x_value, const int y_value);
+		Point(const float x_value, const float y_value);
 		Point &operator=(const Point &other);
-		// Incrementation and Decrementation operators
-		Point &operator++(void);
-		Point operator++(int);
-		Point &operator--(void);
-		Point operator--(int);
-		// Arithmetic operators
-		Point operator*(const Point &other);
-		Point operator/(const Point &other);
-		Point operator+(const Point &other);
-		Point operator-(const Point &other);
-		// Binary operators
-		bool operator<(const Point &rhs) const;
-		bool operator>(const Point &rhs) const;
-		bool operator<=(const Point &rhs) const;
-		bool operator>=(const Point &rhs) const;
-		bool operator==(const Point &rhs) const;
-		bool operator!=(const Point &rhs) const;
 
-		int getRawBits(void) const;
-		void setRawBits(int const raw);
-		float toFloat(void) const;
-		float toInt(void) const;
-		static Point min(const Point &a, const Point &b);
-		static Point min(Point &a, Point &b);
-		static Point max(const Point &a, const Point &b);
-		static Point max(Point &a, Point &b);
+		bool operator==(const Point &rhs) const;
+
+		Fixed getX(void);
+		Fixed getY(void);
 };
 
 std::ostream &operator<<(std::ostream &o, Point const &Point);
